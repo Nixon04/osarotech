81cf1297388619b38876cc6e046d63306b1ac2cc
nixon04 <nixonsampson04@gmail.com>
2025-05-13 13:57:04 +0100

    fixed

--- laravel/resources/views/app.blade.php
+++ laravel/resources/views/app.blade.php
@@ -3,10 +3,8 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @if(app()->environment('production'))
     @vite(['resources/js/app.js', 'resources/js/Pages/front_components/asset/app.css', 'resources/js/Pages/front_components/asset/media.css'])
-    #endif
-    @inertiaHead 
+    @inertiaHead
   </head>
   <body>
     @inertia
